03a3dd04af48877a48d97a5a899c149f08d38794
leonardo <leonardo.rojas@fairplaylabs.com>
2014-10-10 13:33:50 -0600

    Adding test php server.

--- app/view/php/main.php
+++ app/view/php/main.php
@@ -1,8 +1,16 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
+	<head>
+		<title>PHP Test</title>
+		<script type="text/javascript">
+
+		//function functionOne() { alert('You clicked the top text'); }
+		//function functionTwo() { alert('You clicked the bottom text'); }
+
+		//functionOne();
+
+		</script>
+	</head>
+	<body>
+		<?php echo '<p>Hello World</p>'; ?> 
+	</body>
 </html>
