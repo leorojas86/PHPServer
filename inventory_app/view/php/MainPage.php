948cfb14bb712ec91a843590736d7fbce15c8564
leonardo <leonardo.rojas@fairplaylabs.com>
2014-10-22 21:30:06 -0600

    Test PHP server

--- inventory_app/view/php/MainPage.php
+++ inventory_app/view/php/MainPage.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<title>PHP Test</title>
+		<link rel="stylesheet" href="view/css/main_page.css">
 		<script src="utils/js/utils.js" type="text/javascript" > </script>
 		<script type="text/javascript" >
 			
@@ -12,7 +13,39 @@
 	</head>
 	<body>
 		<?php //echo '<p>Hello World</p>'; ?> 
-		<a href="http://localhost:8888?page=MainTestCases">Test Cases</a>
-		<img src="view/images/Folder.png" onclick="alert('Test');" />
+		<div style="overflow:scroll; width:400px; height:200px;" >
+			<div id="folder_icon" style="width:100px; height:120px; float: left;">
+				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
+				<label> Folder Name </label>
+			</div>
+			<div id="folder_icon" style="width:100px; height:120px; float: left;">
+				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
+				<label> Folder Name </label>
+			</div>
+			<div id="folder_icon" style="width:100px; height:120px; float: left;">
+				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
+				<label> Folder Name </label>
+			</div>
+			<div id="folder_icon" style="width:100px; height:120px; float: left;">
+				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
+				<label> Folder Name </label>
+			</div>
+			<div id="folder_icon" style="width:100px; height:120px; float: left;">
+				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
+				<label> Folder Name </label>
+			</div>
+			<div id="folder_icon" style="width:100px; height:120px; float: left;">
+				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
+				<label> Folder Name </label>
+			</div>
+			<div id="folder_icon" style="width:100px; height:120px; float: left;">
+				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
+				<label> Folder Name </label>
+			</div>
+			<div id="folder_icon" style="width:100px; height:120px; float: left;">
+				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
+				<label> Folder Name </label>
+			</div>
+		</div>
 	</body>
 </html>
