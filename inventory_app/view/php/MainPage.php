e318c26d6dd15f42681272cb26580896a26b1335
leonardo <leonardo.rojas@fairplaylabs.com>
2014-10-23 09:00:42 -0600

    Test PHP server

--- inventory_app/view/php/MainPage.php
+++ inventory_app/view/php/MainPage.php
@@ -10,10 +10,33 @@
 			//showAlert();
 
 		</script>
+		<!-- http://stackoverflow.com/questions/4909167/how-to-add-a-custom-right-click-menu-to-a-webpage -->
+		<!-- http://www.codeproject.com/Tips/630793/Context-Menu-on-Right-Click-in-Webpage -->
+		<script type="text/javascript">
+		    if (document.addEventListener) {
+		        document.addEventListener('contextmenu', function(e) {
+		            alert("You've tried to open context menu"); //here you draw your own menu
+		            e.preventDefault();
+		        }, false);
+		    } else {
+		        document.attachEvent('oncontextmenu', function() {
+		            alert("You've tried to open context menu");
+		            window.event.returnValue = false;
+		        });
+		    }
+		</script>
 	</head>
 	<body>
 		<?php //echo '<p>Hello World</p>'; ?> 
-		<div style="overflow:scroll; width:400px; height:200px;" >
+		<div style="overflow:scroll; width:450px; height:300px; margin-left: 300px;" >
+			<div id="folder_icon" style="width:100px; height:120px; float: left;">
+				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
+				<label> Folder Name </label>
+			</div>
+			<div id="folder_icon" style="width:100px; height:120px; float: left;">
+				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
+				<label> Folder Name </label>
+			</div>
 			<div id="folder_icon" style="width:100px; height:120px; float: left;">
 				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
 				<label> Folder Name </label>
