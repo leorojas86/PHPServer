327bfc54baa4f084aca654f540be5187c10a9819
leonardo <leonardo.rojas@fairplaylabs.com>
2014-10-23 21:52:05 -0600

    Test PHP server

--- inventory_app/utils/js/utils.js
+++ inventory_app/utils/js/utils.js
@@ -43,6 +43,25 @@ function request(url, params, method, callback)
 	}
 }
 
+function disableDefaultContextMenu()
+{
+	if (document.addEventListener) 
+    {
+        document.addEventListener('contextmenu', function(e) {
+            //alert("You've tried to open context menu"); //here you draw your own menu
+            e.preventDefault();
+        }, false);
+    } 
+    else 
+    {
+        document.attachEvent('oncontextmenu', function() 
+        {
+            //alert("You've tried to open context menu");
+            window.event.returnValue = false;
+        });
+    }
+}
+
 function checkForValidResponse(xmlhttp)
 {
 	return xmlhttp.readyState == 4 && xmlhttp.status == 200;
--- inventory_app/view/php/MainPage.php
+++ inventory_app/view/php/MainPage.php
@@ -4,40 +4,35 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<title>PHP Test</title>
 		<link rel="stylesheet" href="view/css/main_page.css">
-		<script src="utils/js/utils.js" type="text/javascript" > </script>
-		<script type="text/javascript" >
-			
-			//showAlert();
-
-		</script>
+		<script src="utils/js/utils.js" type="text/javascript" ></script>
 		<!-- http://stackoverflow.com/questions/4909167/how-to-add-a-custom-right-click-menu-to-a-webpage -->
 		<!-- http://www.codeproject.com/Tips/630793/Context-Menu-on-Right-Click-in-Webpage -->
 		<script type="text/javascript">
-		    if (document.addEventListener) {
-		        document.addEventListener('contextmenu', function(e) {
-		            alert("You've tried to open context menu"); //here you draw your own menu
-		            e.preventDefault();
-		        }, false);
-		    } else {
-		        document.attachEvent('oncontextmenu', function() {
-		            alert("You've tried to open context menu");
-		            window.event.returnValue = false;
-		        });
+
+			onload = function() 
+		    {
+    			disableDefaultContextMenu();
+			}
+
+		    function ShowMenu(control, e) 
+		    {
+		    	alert("ShowMenu");
 		    }
+
 		</script>
 	</head>
 	<body>
 		<?php //echo '<p>Hello World</p>'; ?> 
 		<div style="overflow:scroll; width:450px; height:300px; margin-left: 300px;" >
-			<div id="folder_icon" style="width:100px; height:120px; float: left;">
+			<div id="folder_icon" style="width:100px; height:120px; float: left;" oncontextmenu="ShowMenu('contextMenu',event);">
 				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
 				<label> Folder Name </label>
 			</div>
-			<div id="folder_icon" style="width:100px; height:120px; float: left;">
+			<div id="folder_icon" style="width:100px; height:120px; float: left;" oncontextmenu="ShowMenu('contextMenu',event);">
 				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
 				<label> Folder Name </label>
 			</div>
-			<div id="folder_icon" style="width:100px; height:120px; float: left;">
+			<div id="folder_icon" style="width:100px; height:120px; float: left;" oncontextmenu="ShowMenu('contextMenu',event);">
 				<img src="view/images/Folder.png" onclick="alert('Test');" style="cursor:pointer; cursor:hand;"/>
 				<label> Folder Name </label>
 			</div>
