fb619b25fd1432cf8b9dd94f90dc363c1ccca511
Leonardo Rojas <leonardo.rojas@fairplaylabs.com>
2015-01-20 16:52:21 -0600

    Adding template html.

---
+++ app_template/view/php/PageTemplate.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<title>PHP Test</title>
+		<!--<script src="utils/js/utils.js" type="text/javascript"> </script>-->
+		<script type="text/javascript">
+			
+			function onPageLoaded()
+			{
+				alert("Page Loaded");
+			}
+
+		</script>
+	</head>
+	<body onload="onPageLoaded();">
+		<?php echo '<p>Hello World</p>'; ?> 
+	</body>
+</html>
