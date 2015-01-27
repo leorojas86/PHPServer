22fbcdcff20bac1d62fce8427f54f5f5c0200a10
Leonardo Rojas <leonardo.rojas@fairplaylabs.com>
2015-01-27 12:13:09 -0600

    Implementing release notes example.

--- ReleaseNotes/jira_utils.js
+++ ReleaseNotes/jira_utils.js
@@ -28,6 +28,30 @@ JiraUtilsClass.prototype.initialize = function(jiraSettings)
 
 JiraUtilsClass.prototype.replaceJiraTags = function(plainText, onAllTagsReplaced)
 {
-	
+	while(this.replaceNextJiraTag(plainText, function(resultText) { plainText = resultText; }))
+	{
+
+	}
+
 	onAllTagsReplaced(plainText);
 };
+
+JiraUtilsClass.prototype.replaceNextJiraTag = function(plainText, onNextTagReplaced)
+{
+	var tagFirstIndex = plainText.indexOf("{");
+
+	if(tagFirstIndex != -1)
+	{
+		var tagLastIndex = plainText.indexOf("}");
+		var tag 		 = plainText.substring(tagFirstIndex, tagLastIndex + 1);
+		plainText 		 = plainText.replace(tag, "Replaced");
+
+		onNextTagReplaced(plainText);
+
+		return true;
+	}
+
+	onNextTagReplaced(plainText);
+
+	return false;
+}
