e56af9a0f6455fb90c0565207156c88fe6a0bdb8
Leonardo Rojas <leonardo.rojas@fairplaylabs.com>
2015-11-28 14:44:24 -0600

    Improving contants template.

--- template/constants_template.js
+++ template/constants_template.js
@@ -1,6 +1,6 @@
 
 var ConstantsTemplate =
 {
-     'TEMPLATE_CONSTANT_1' : true,
-     'TEMPLATE_CONSTANT_2' : { "w" : 380, "h" : 520 }
+     TEMPLATE_CONSTANT_1 : true,
+     TEMPLATE_CONSTANT_2 : { "w" : 380, "h" : 520 }
 };
