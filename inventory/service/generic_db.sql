241a5c7cadbb7eb2f9e87e01a61932d1b2d001dc
Leo <leorojas86@gmail.com>
2016-03-08 20:32:25 -0600

    Implementing php server.

--- inventory/service/generic_db.sql
+++ inventory/service/generic_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Jan 25, 2016 at 04:44 AM
+-- Generation Time: Mar 09, 2016 at 03:30 AM
 -- Server version: 5.5.42
 -- PHP Version: 7.0.0
 
@@ -26,29 +26,78 @@ CREATE TABLE `groups` (
   `type` int(11) NOT NULL,
   `user_id` bigint(20) NOT NULL,
   `parent_group_id` bigint(20) DEFAULT NULL,
-  `tags` varchar(300) DEFAULT NULL,
   `data` varchar(1000) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=135 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `groups`
 --
 
-INSERT INTO `groups` (`id`, `name`, `type`, `user_id`, `parent_group_id`, `tags`, `data`) VALUES
-(1, 'sdsds', 1, 8, 44, NULL, NULL),
-(42, 'RootGroup', 0, 8, 0, NULL, NULL),
-(44, 'test1', 0, 8, 42, NULL, NULL),
-(50, 'RootGroup', 0, 10, 0, NULL, NULL),
-(54, '233', 1, 8, 42, NULL, '312'),
-(55, '2323', 0, 8, 42, NULL, NULL),
-(56, 'sasd', 0, 8, 42, NULL, NULL),
-(57, 'ssss', 1, 8, 42, NULL, NULL),
-(59, 'item', 1, 8, 56, NULL, NULL),
-(60, 'sss', 1, 8, 42, NULL, NULL),
-(61, '2332', 0, 8, 42, NULL, NULL),
-(62, 'adasda', 0, 8, 55, NULL, NULL),
-(63, 'asdasdasd', 0, 8, 62, NULL, NULL),
-(64, 'ssss', 1, 8, 42, NULL, NULL);
+INSERT INTO `groups` (`id`, `name`, `type`, `user_id`, `parent_group_id`, `data`) VALUES
+(1, 'sdsds', 1, 8, 44, NULL),
+(42, 'RootGroup', 0, 8, 0, NULL),
+(44, 'test111', 0, 8, 42, NULL),
+(50, 'RootGroup', 0, 10, 0, NULL),
+(60, 'sss', 1, 8, 42, '555sss'),
+(61, 'dfdfdf', 0, 8, 42, NULL),
+(65, 'ddddd', 1, 8, 44, 'nullsdd'),
+(67, 'sdsd', 0, 8, 44, NULL),
+(68, 'sdsd', 0, 8, 67, NULL),
+(70, 'ssss', 0, 8, 42, NULL),
+(71, 'ssaaa', 1, 8, 42, NULL),
+(73, 'sasdds', 1, 8, 42, 'nullsss'),
+(74, 'aasddsdsa', 1, 8, 42, NULL),
+(76, 'sdasddddd', 0, 8, 76, NULL),
+(79, 'fdffddf', 0, 8, 70, NULL),
+(80, 'RootGroup', 0, 14, 0, NULL),
+(81, 'r5trtt', 1, 14, 80, NULL),
+(82, 'vgvh', 0, 8, 79, NULL),
+(83, 'RootGroup', 0, 0, 0, NULL),
+(84, 'dd', 1, 8, 42, NULL),
+(85, 'sds', 0, 8, 42, NULL),
+(86, 'dfdf', 1, 8, 42, NULL),
+(87, 'dddd', 0, 8, 61, NULL),
+(88, 'sssssaaaaa', 1, 8, 42, NULL),
+(89, 'sssssaaaaaa', 0, 8, 89, NULL),
+(90, 'sadasd', 1, 0, 83, 'www'),
+(91, 'asdasd', 0, 0, 83, NULL),
+(92, 'sdddsssss', 1, 0, 91, NULL),
+(93, 'new', 1, 8, 76, NULL),
+(94, 'w', 1, 8, 42, NULL),
+(95, 'w', 1, 8, 42, NULL),
+(96, 'w', 1, 8, 42, NULL),
+(97, 'w', 1, 8, 42, NULL),
+(98, 'w', 1, 8, 42, NULL),
+(99, 'w', 1, 8, 42, NULL),
+(100, 'w', 1, 8, 42, NULL),
+(101, 'w', 1, 8, 42, NULL),
+(102, 'w', 1, 8, 42, NULL),
+(104, 'w', 1, 8, 42, NULL),
+(105, 'w', 1, 8, 42, NULL),
+(106, 'w', 1, 8, 42, NULL),
+(107, 'w', 1, 8, 42, NULL),
+(108, 'w', 1, 8, 42, NULL),
+(109, 'w', 1, 8, 42, NULL),
+(110, 'w', 1, 8, 42, NULL),
+(111, 'w', 1, 8, 42, NULL),
+(112, 'w', 1, 8, 42, NULL),
+(113, 'w', 1, 8, 42, NULL),
+(114, 'w', 1, 8, 42, NULL),
+(115, 'w', 1, 8, 42, NULL),
+(116, 'w', 1, 8, 42, NULL),
+(117, 'w', 1, 8, 42, NULL),
+(118, 'w', 1, 8, 42, NULL),
+(119, '1', 1, 8, 42, NULL),
+(124, 'a', 1, 8, 42, NULL),
+(126, 'a', 1, 8, 42, NULL),
+(127, 'a', 1, 8, 42, NULL),
+(128, 'a', 1, 8, 42, NULL),
+(129, 'a', 1, 8, 42, NULL),
+(130, 'a', 1, 8, 42, NULL),
+(131, 'a', 1, 8, 42, NULL),
+(132, 'a', 1, 8, 42, NULL),
+(133, 'a', 1, 8, 42, NULL),
+(134, 'sss', 0, 8, 85, NULL);
 
 -- --------------------------------------------------------
 
@@ -58,7 +107,19 @@ INSERT INTO `groups` (`id`, `name`, `type`, `user_id`, `parent_group_id`, `tags`
 
 CREATE TABLE `tags` (
   `id` bigint(20) NOT NULL,
-  `name` varchar(100) NOT NULL
+  `name` varchar(100) NOT NULL,
+  `type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags_per_groups`
+--
+
+CREATE TABLE `tags_per_groups` (
+  `group_id` bigint(20) NOT NULL,
+  `tag_id` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -74,7 +135,7 @@ CREATE TABLE `users` (
   `password` varchar(100) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
   `data` varchar(2000) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -86,7 +147,8 @@ INSERT INTO `users` (`id`, `fb_id`, `email`, `password`, `name`, `data`) VALUES
 (10, NULL, 'leos', 'leos', 'leos', NULL),
 (11, NULL, '3', '3', '3', NULL),
 (12, NULL, '5', '5', '5', NULL),
-(13, NULL, 'leo1', 'leo1', 'leo1', NULL);
+(13, NULL, 'leo1', 'leo1', 'leo1', NULL),
+(14, NULL, 'leod', 'leod', 'leod', NULL);
 
 --
 -- Indexes for dumped tables
@@ -105,6 +167,12 @@ ALTER TABLE `tags`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tags_per_groups`
+--
+ALTER TABLE `tags_per_groups`
+  ADD PRIMARY KEY (`group_id`,`tag_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -118,7 +186,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=65;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=135;
 --
 -- AUTO_INCREMENT for table `tags`
 --
@@ -128,4 +196,4 @@ ALTER TABLE `tags`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
