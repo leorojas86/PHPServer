bc18248b599593a6bb17d2c9b2267555c8cbf50f
Leo <leorojas86@gmail.com>
2016-03-16 21:23:41 -0600

    Implementing php server.

--- inventory/html_client/js/popups/search_popup.js
+++ inventory/html_client/js/popups/search_popup.js
@@ -11,9 +11,11 @@ SearchPopupClass.prototype.show = function()
 {
 	var searchText 		= LocManager.instance.getLocalizedText("search_button_text");
 	var defaultValue 	= "";
-	var html 	   		= "<div class='search_popup_class popup_container'>" + 
-						  	"<input 	type='text' id='search_input_text' 		class='input_class margin_class' value = '" + defaultValue + "'>" +
-					 	  	"<button 				id='search_popup_button'	class='button_class margin_class'>" + searchText + "</button>" +
+	var html 	   		= "<div class='search_popup_class' align='center'>" + 
+							"<div class='search_popup_content_class'>" +
+						  		"<input 	type='text' id='search_input_text' 		class='input_class margin_class' value = '" + defaultValue + "'>" +
+					 	  		"<button 				id='search_popup_button'	class='button_class margin_class'>" + searchText + "</button>" +
+					 	  	"<div>" +
 					 	  "</div>";
 
 	document.getElementById("search_popup_container").innerHTML = html;
