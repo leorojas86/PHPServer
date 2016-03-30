5f8f14096e57fb49251d5bbaea60e986bbb5a01a
Leo <leorojas86@gmail.com>
2016-03-29 22:31:11 -0600

    Implementing php server.

---
+++ inventory/ios_client/Inventory/Inventory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Inventory
+//
+//  Created by Leo on 3/29/16.
+//  Copyright © 2016 Leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ inventory/ios_client/Inventory/Inventory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Inventory
+//
+//  Created by Leo on 3/29/16.
+//  Copyright © 2016 Leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
