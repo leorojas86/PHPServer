843aeaff4a23e4bf3b6ac80277e461fbf2019231
Leo <leorojas86@gmail.com>
2016-04-17 13:56:11 -0600

    Implementing php server.

--- inventory/html_client/js/controllers/inventory_controller.js
+++ inventory/html_client/js/controllers/inventory_controller.js
@@ -17,7 +17,7 @@ InventoryControllerClass.prototype.render = function()
 	var loadingText 		= LocManager.instance.getLocalizedText("loading_text");
 	var pageContainer 		= document.getElementById("page_container");
 	pageContainer.innerHTML	= 	"<div id='inventory_container' align='center'>" +
-									"<div id='group_container'>" + loadingText + "</div>" +
+									"<div id='group_container' class='group_container_class' >" + loadingText + "</div>" +
 				   	 			"</div>";
 
 	InventoryGroupController.instance.renderRootGroup();
