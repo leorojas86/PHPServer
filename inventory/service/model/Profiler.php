ac500638cda733b3cb3cb27d97e25e2f668c2549
Leo <leorojas86@gmail.com>
2016-05-07 14:46:27 -0600

    Implementing php server.

--- inventory/service/model/Profiler.php
+++ inventory/service/model/Profiler.php
@@ -8,36 +8,19 @@
 		public function __construct($name)
 		{
 			$this->name 			= $name;
-			$this->profileStartTime = $this->GetCurrentMiliseconds();
+			$this->profileStartTime = microtime(true);
 
-			// Sleep for a while
-			usleep(200000);
+			//usleep(2000000);// Sleep for a while
 		}
 
 		public function Finish()
 		{
-			$currentMilliseconds 	= $this->GetCurrentMiliseconds();
+			$currentMilliseconds 	= microtime(true);
 			$profileDuration 		= $currentMilliseconds - $this->profileStartTime;
 
 			error_log("Profile '$this->name', duration = '$profileDuration', startTime = '$this->profileStartTime', endTime = '$currentMilliseconds'");
 
 			return $profileDuration;
-		}
-
-		public function GetCurrentMiliseconds()
-		{
-			$currentMilliseconds = microtime(true) / 100000;
-			return $currentMilliseconds;
-
-			/*$microtime = microtime();
-  $comps = explode(' ', $microtime);
-
-  // Note: Using a string here to prevent loss of precision
-  // in case of "overflow" (PHP converts it to a double)
-  return sprintf('%d%03d', $comps[1], $comps[0] * 1000);*/
-
-    /*list($usec, $sec) = explode(" ", microtime());
-    return ((float)$usec + (float)$sec);*/
 		}
 	}
 ?>
