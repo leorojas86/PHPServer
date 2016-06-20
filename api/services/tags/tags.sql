cef8b448050c97139827831577499052c1314265
Leo <leorojas86@gmail.com>
2016-06-19 21:04:14 -0600

    Implementing php server.

--- api/services/tags/tags.sql
+++ api/services/tags/tags.sql
@@ -6,12 +6,12 @@ CREATE TABLE `tags`
 (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `type` int(11) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `tags_per_id` 
 (
-  `id` bigint(20) NOT NULL,
-  `tag_id` bigint(20) NOT NULL
+  `tag_id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL, -- id associated to the tag
+  `type` int(11) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
