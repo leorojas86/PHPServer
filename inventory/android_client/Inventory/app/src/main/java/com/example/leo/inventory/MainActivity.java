f9166a1fff0c1fb511675363dd3702081f5d370c
Leo <leorojas86@gmail.com>
2016-07-06 19:37:34 -0600

    Implementing php server.

--- inventory/android_client/Inventory/app/src/main/java/com/example/leo/inventory/MainActivity.java
+++ inventory/android_client/Inventory/app/src/main/java/com/example/leo/inventory/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.leo.inventory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        //webView.addJavascriptInterface(new JSInterface(this), "external");
+//webView.loadUrl("http://google.com");
+        webView.loadUrl("http://201.200.1.187/inventory/html_client");
     }
 }
