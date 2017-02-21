ee15edbd5887c6d64a8a82f2fcaafff6328b7139
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-02-20 21:43:39 -0600

    Implementing PHP server

--- api/services/tags/DBConfig.php
+++ api/services/tags/DBConfig.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 	class DBConfig
 	{
 		const DB_SERVER  = 'localhost';
 		const DB_USER 	 = 'root';
-		const DB_PASS 	 = 'root';
+		const DB_PASS 	 = '';
 		const DB_NAME 	 = 'tags';
 		const DB_PORT    = '8889';//Default is 3306
 	}
