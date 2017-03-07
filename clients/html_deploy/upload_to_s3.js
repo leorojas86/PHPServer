90f6dcc25abbbd723a3c088b46b9166111305ee5
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-03-06 18:59:12 -0600

    Implementing PHP server

--- clients/html_deploy/upload_to_s3.js
+++ clients/html_deploy/upload_to_s3.js
@@ -1,23 +1,27 @@
 //https://aws.amazon.com/sdk-for-node-js/
 
-var AWS = require('aws-sdk');
+var AWS    = require('aws-sdk');
 var async = require('async');
-var fs = require('fs');
-var path = require("path");
-var s3 = new AWS.S3();
+var fs    = require('fs');
+var path  = require("path");
+var s3    = new AWS.S3();
 
-var myBucket = 'aws-website-crnegocioscom-yxnfk';
-var deployFolder = path.resolve('./deploy');
+var BUCKET        = 'aws-website-crnegocioscom-yxnfk';
+var DEPLOY_FOLDER = path.resolve('./deploy');
 
-function createBucket() {
-  s3.createBucket({Bucket: myBucket}, function(err, data) {
-    if (err) { console.log(err); }
+function createBucket(bucketFolder) {
+  s3.createBucket({ Bucket: bucketFolder }, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully created bucket folder " + bucketFolder); 
+    }
   });
 }
 
 function uploadFileToS3(file, filePath) {
   console.log('Uploading file -> ' + file);
-  var params = { Bucket: myBucket, Key: file, Body: fs.readFileSync(filePath) };
+  var params = { Bucket: BUCKET, Key: file, Body: fs.readFileSync(filePath) };
   s3.putObject(params, function(err, data) {
     if (err) {
       console.log(err)
@@ -34,11 +38,14 @@ function uploadFolder(folder) {
     if(fs.lstatSync(filePath).isFile()) {
       uploadFileToS3(file, filePath);
     }
+    else {
+      createBucket(BUCKET + '/' + file);
+    }
   }, function (err, results) {
     if (err) console.error(err);
     console.log(results);
   });
 }
 
-createBucket();
-uploadFolder(deployFolder);
+createBucket(BUCKET);
+uploadFolder(DEPLOY_FOLDER);
