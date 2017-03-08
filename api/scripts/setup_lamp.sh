5be52092bdd86974726935baf8dd2e623250f94b
Leo <leonardo.rojas@agilysys.com>
2017-03-07 21:22:55 -0600

    Implementing PHP server

--- api/scripts/setup_lamp.sh
+++ api/scripts/setup_lamp.sh
@@ -1,10 +1,20 @@
 echo '
 
 
-Updating apt-get'
+-> Updating apt-get'
 sudo apt-get update
 echo '
 
 
-installing apache2'
+-> Installing apache'
 sudo apt-get install apache2
+echo '
+
+
+-> Installing mysql'
+sudo apt-get install mysql-server
+echo '
+
+
+-> Installing php'
+sudo apt-get install php5 libapache2-mod-php5
