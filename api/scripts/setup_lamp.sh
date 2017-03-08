64a4c3b16986775385b81917e6775e411c270ea1
Leo <leonardo.rojas@agilysys.com>
2017-03-07 21:35:10 -0600

    Implementing PHP server

--- api/scripts/setup_lamp.sh
+++ api/scripts/setup_lamp.sh
@@ -1,20 +1,21 @@
+# https://www.atlantic.net/community/howto/install-linux-apache-mysql-php-lamp-stack-on-ubuntu-16-04/
 echo '
 
 
 -> Updating apt-get'
-sudo apt-get update
+sudo apt update
 echo '
 
 
 -> Installing apache'
-sudo apt-get install apache2
+sudo apt install apache2
 echo '
 
 
 -> Installing mysql'
-sudo apt-get install mysql-server
+sudo apt install mysql-server php7.0-mysql
 echo '
 
 
 -> Installing php'
-sudo apt-get install php5 libapache2-mod-php5
+apt install php libapache2-mod-php
