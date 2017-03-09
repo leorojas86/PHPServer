c900ccb898f020ba78e04a17eb21eb1c1710d096
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-03-08 21:36:03 -0600

    Implementing PHP server

--- api/scripts/setup_lamp.sh
+++ api/scripts/setup_lamp.sh
@@ -3,23 +3,23 @@
 echo '
 
 
--> Updating apt-get'
+-> Updating apt-get...'
 sudo bash -c 'yes | sudo apt update'
 sudo bash -c 'yes | sudo apt upgrade'
 echo '
 
 
--> Installing apache'
+-> Installing apache...'
 sudo bash -c 'yes | sudo apt install apache2'
 echo '
 
 
--> Installing mysql'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root123'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root123'
+-> Installing mysql..'
+echo "mysql-server mysql-server/root_password password root123" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root123" | debconf-set-selections
 sudo bash -c 'yes | apt install mysql-server php7.0-mysql'
 echo '
 
 
--> Installing php'
+-> Installing php...'
 sudo apt install php libapache2-mod-php
