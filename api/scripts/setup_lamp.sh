324f420bb39759cad7ba3afb77e8196bbbf18f17
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-03-09 22:09:43 -0600

    Implementing PHP server

--- api/scripts/setup_lamp.sh
+++ api/scripts/setup_lamp.sh
@@ -17,7 +17,7 @@ sudo cp -rf "virtual_hosts/api.conf" "/etc/apache2/sites-available/api.conf"
 sudo cp -rf "virtual_hosts/inventory.conf" "/etc/apache2/sites-available/inventory.conf"
 PWD=`pwd`
 #REPO_PATH="$PWD/../.."
-REPO_PATH=$(sed 's#api/scripts##g' <<< $PWD)
+REPO_PATH=$(sed 's#api/scripts# #g' <<< $PWD)
 sudo sed -i "s#{{REPO_PATH}}#$REPO_PATH#g" "/etc/apache2/sites-available/api.conf"
 sudo sed -i "s#{{REPO_PATH}}#$REPO_PATH#g" "/etc/apache2/sites-available/inventory.conf"
 echo "
