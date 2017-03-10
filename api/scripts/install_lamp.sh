088ca39ee583acb4a3ea87578588d2bdbc05a4cf
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-03-09 21:25:37 -0600

    Implementing PHP server

--- api/scripts/install_lamp.sh
+++ api/scripts/install_lamp.sh
@@ -9,34 +9,34 @@ echo "
 Script location: $MY_PATH"
 cd "$MY_PATH"
 pwd
-echo '
+echo "
 
 
--> Updating apt-get...'
-sudo bash -c 'yes | sudo apt update'
-sudo bash -c 'yes | sudo apt upgrade'
-echo '
+-> Updating apt-get..."
+sudo bash -c "yes | sudo apt update"
+sudo bash -c "yes | sudo apt upgrade"
+echo "
 
 
--> Installing apache...'
-sudo bash -c 'yes | sudo apt install apache2'
-echo '
+-> Installing apache..."
+sudo bash -c "yes | sudo apt install apache2"
+echo "
 
 
--> Installing mysql...'
+-> Installing mysql..."
 #sudo dpkg --configure -a
 echo "mysql-server mysql-server/root_password password root123" | sudo debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password root123" | sudo debconf-set-selections
-sudo bash -c 'yes | apt -y install mysql-server php7.0-mysql'
-echo '
+sudo bash -c "yes | apt -y install mysql-server php7.0-mysql"
+echo "
 
 
--> Installing php...'
-sudo bash -c 'yes | sudo apt install php libapache2-mod-php'
-echo '
+-> Installing php..."
+sudo bash -c "yes | sudo apt install php libapache2-mod-php"
+echo "
 
 
--> Running MySQL Scripts...'
+-> Running MySQL Scripts..."
 mysql --user="root" --password="root123" < "../services/users/users.sql"
 mysql --user="root" --password="root123" < "../services/tags/tags.sql"
 mysql --user="root" --password="root123" < "../services/groups/groups.sql"
--- api/scripts/setup_lamp.sh
+++ api/scripts/setup_lamp.sh
@@ -8,13 +8,13 @@ echo "
 Script location: $MY_PATH"
 cd "$MY_PATH"
 pwd
-echo '
+echo "
 
 
--> Updating virtual hosts...'
-sudo cp -rf 'virtual_hosts/api.conf' '/etc/apache2/sites-available/api.conf'
-sudo cp -rf 'virtual_hosts/inventory.conf' '/etc/apache2/sites-available/inventory.conf'
+-> Updating virtual hosts..."
+sudo cp -rf "virtual_hosts/api.conf" "/etc/apache2/sites-available/api.conf"
+sudo cp -rf "virtual_hosts/inventory.conf" "/etc/apache2/sites-available/inventory.conf"
 REPO_PATH=echo "$MY_PATH" | sed "s/'api/scripts'//"
-echo 'REPO_PATH=$REPO_PATH'
-sudo sed -i "s/{{REPO_PATH}}/$REPO_PATH/g" '/etc/apache2/sites-available/api.conf'
-sudo sed -i "s/{{REPO_PATH}}/$REPO_PATH/g" '/etc/apache2/sites-available/inventory.conf'
+echo "REPO_PATH=$REPO_PATH"
+sudo sed -i "s/{{REPO_PATH}}/$REPO_PATH/g" "/etc/apache2/sites-available/api.conf"
+sudo sed -i "s/{{REPO_PATH}}/$REPO_PATH/g" "/etc/apache2/sites-available/inventory.conf"
