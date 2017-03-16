b93d883711e7cbe975c6f84c3e2f98e0caf46299
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-03-16 17:02:57 -0600

    Implementing PHP server

--- api/scripts/install_docker.sh
+++ api/scripts/install_docker.sh
@@ -34,7 +34,7 @@ echo "
 --- Verifying that the key fingerprint is 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88"
 sudo apt-key fingerprint 0EBFCD88
 echo "
---- Set up the stable repository"
+--- Setting up the stable repository"
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
