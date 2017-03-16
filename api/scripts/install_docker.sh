d2bc434412652c56f5458d28416bea77f8db9961
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-03-16 16:48:24 -0600

    Implementing PHP server

--- api/scripts/install_docker.sh
+++ api/scripts/install_docker.sh
@@ -18,3 +18,15 @@ sudo bash -c "yes | sudo apt upgrade"
 echo "
 
 -> Installing docker..."
+
+#https://docs.docker.com/engine/installation/linux/ubuntu/#install-using-the-repository
+#Install packages to allow apt to use a repository over HTTPS
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+#Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#Verify that the key fingerprint is 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88
+sudo apt-key fingerprint 0EBFCD88
