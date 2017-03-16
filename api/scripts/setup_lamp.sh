8220b154de1520af607aa3bc36df820341934531
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-03-15 18:49:14 -0600

    Implementing PHP server

--- api/scripts/setup_lamp.sh
+++ api/scripts/setup_lamp.sh
@@ -11,6 +11,13 @@ cd "$MY_PATH"
 pwd
 echo "
 
+#https://pe.godaddy.com/help/configure-apache-virtual-hosts-ubuntu-17336
+#https://www.digitalocean.com/community/tutorials/how-to-set-up-apache-virtual-hosts-on-ubuntu-14-04-lts
+#https://support.rackspace.com/how-to/set-up-apache-virtual-hosts-on-ubuntu/
+#https://tecadmin.net/create-virtual-hosts-in-apache-on-ubuntu/
+#https://www.unixmen.com/setup-apache-virtual-hosts-on-ubuntu-15-10/
+#https://www.vultr.com/docs/apache-virtual-hosts-on-ubuntu-14-04-lts
+#https://httpd.apache.org/docs/2.4/vhosts/examples.html
 
 -> Updating virtual hosts..."
 sudo cp -rf "virtual_hosts/api.conf" "/etc/apache2/sites-available/api.conf"
@@ -21,30 +28,31 @@ REPO_PATH=$(echo ${PWD} | sed 's#/api/scripts##g')
 sudo sed -i "s#{{REPO_PATH}}#$REPO_PATH#g" "/etc/apache2/sites-available/api.conf"
 sudo sed -i "s#{{REPO_PATH}}#$REPO_PATH#g" "/etc/apache2/sites-available/inventory.conf"
 
-sudo a2ensite api.conf
-sudo a2ensite inventory.conf
+#sudo a2ensite api.conf
+#sudo a2ensite inventory.conf
 
-<<"COMMENT"
-if grep -Fxq "api.conf" /etc/apache2/ports.conf
-then
-  echo "ports.conf file is already updated"
-else
-  sudo echo "
-NameVirtualHost *:80
+sudo a2dissite api.conf
+sudo a2dissite inventory.conf
 
-<IfModule mod_ssl.c>
-  # SSL name based virtual hosts are not yet supported, therefore no
-  /etc/apache2/sites-available/api.conf
-  Listen 80
-</IfModule>
-<IfModule mod_ssl.c>
+#if grep -Fxq "api.conf" /etc/apache2/ports.conf
+#then
+#  echo "ports.conf file is already updated"
+#else
+#  sudo echo "
+#NameVirtualHost *:80
+
+#<IfModule mod_ssl.c>
+#  # SSL name based virtual hosts are not yet supported, therefore no
+#  /etc/apache2/sites-available/api.conf
+#  Listen 80
+#</IfModule>
+#<IfModule mod_ssl.c>
   # SSL name based virtual hosts are not yet supported, therefore no
   /etc/apache2/sites-available/inventory.conf
   Listen 80
-</IfModule>
-  " >> /etc/apache2/ports.conf
-fi
-COMMENT
+#</IfModule>
+#  " >> /etc/apache2/ports.conf
+#fi
 
 echo "
 
