bff1d68355532e3b279754ef3c6632a78a434cc8
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-03-16 09:54:37 -0600

    Implementing PHP server

--- api/scripts/setup_lamp.sh
+++ api/scripts/setup_lamp.sh
@@ -20,33 +20,29 @@ echo "
 #https://httpd.apache.org/docs/2.4/vhosts/examples.html
 
 -> Updating virtual hosts..."
-sudo cp -rf "virtual_hosts/api.conf" "/etc/apache2/sites-available/api.conf"
-sudo cp -rf "virtual_hosts/inventory.conf" "/etc/apache2/sites-available/inventory.conf"
+sudo cp -rf "virtual_hosts/000-default.conf" "/etc/apache2/sites-available/000-default.conf"
 PWD=`pwd`
 sudo chmod -R 755 "$PWD"
 REPO_PATH=$(echo ${PWD} | sed 's#/api/scripts##g')
-sudo sed -i "s#{{REPO_PATH}}#$REPO_PATH#g" "/etc/apache2/sites-available/api.conf"
-sudo sed -i "s#{{REPO_PATH}}#$REPO_PATH#g" "/etc/apache2/sites-available/inventory.conf"
+sudo sed -i "s#{{REPO_PATH}}#$REPO_PATH#g" "/etc/apache2/sites-available/000-default.conf"
 
-sudo a2ensite api.conf
-sudo a2ensite inventory.conf
-#sudo a2dissite api.conf
-#sudo a2dissite inventory.conf
+sudo a2dissite api.conf
+sudo a2dissite inventory.conf
 
-echo "
-
--> Updating hosts file..."
-if grep -Fxq "127.0.0.1	api" /etc/hosts
-then
-  echo "hosts file is already updated"
-else
-  sudo echo "
-
-  127.0.0.1	api
-  127.0.0.1	inventory
-  " >> /etc/hosts
-fi
-echo "
+#echo "
+#
+#-> Updating hosts file..."
+#if grep -Fxq "127.0.0.1	api" /etc/hosts
+#then
+#  echo "hosts file is already updated"
+#else
+#  sudo echo "
+#
+#  127.0.0.1	api
+#  127.0.0.1	inventory
+#  " >> /etc/hosts
+#fi
+#echo "
 
 
 -> Restarting Apache..."
