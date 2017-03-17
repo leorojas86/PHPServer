33360f350e844153311a1b98478370db7a5292f5
Leo <leonardo.rojas@agilysys.com>
2017-03-16 18:55:44 -0600

    Implementing PHP server

--- api/scripts/install_docker.sh
+++ api/scripts/install_docker.sh
@@ -39,3 +39,6 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+ echo "
+ --- Installing docker CE"
+ sudo apt install docker-ce
