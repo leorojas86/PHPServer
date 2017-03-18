1f2ae3d99722830e724a9b6ac9005d925c7a0681
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-03-17 20:48:20 -0600

    Implementing PHP server

---
+++ scripts/docker/2_create_image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear && clear && clear && clear
+MY_PATH=`dirname "$0"`
+MY_NAME=`basename "$0"`
+
+echo "
+
+Script location: $MY_PATH"
+cd "$MY_PATH"
+pwd
+
+echo "
+
+-> Updating apt-get..."
