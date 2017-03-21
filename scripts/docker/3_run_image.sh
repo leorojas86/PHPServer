3674dbecf04697a68d710c962972ffc44bec7381
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-03-20 21:03:44 -0600

    Implementing PHP server

---
+++ scripts/docker/3_run_image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear && clear && clear && clear
+MY_PATH=`dirname "$0"`
+MY_NAME=`basename "$0"`
+
+echo "
+
+Script location: $MY_PATH"
+cd "$MY_PATH"
+pwd
+
+echo "
+
+-> Seeing all Docker images:"
+docker images --all
+echo "
+
+-> Running image..."
+sudo docker run leotest
