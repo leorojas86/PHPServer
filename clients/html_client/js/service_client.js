de2db832ac6481d0ee6e3ef05cb4d14f78c98be5
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-03-22 21:01:38 -0600

    Implementing PHP server

--- clients/html_client/js/service_client.js
+++ clients/html_client/js/service_client.js
@@ -48,8 +48,7 @@ function ServiceClientClass()
 	{
 		this.profile(params, duration);
 
-		//alert(xmlhttp.responseText);
-		alert('xmlhttp.responseText' + xmlhttp.responseText);
+		//alert('xmlhttp.responseText' + xmlhttp.responseText);
 		var resultData = success ? JSON.parse(xmlhttp.responseText) : { success : false, data : xmlhttp.responseText };
 
 		callback(resultData);
