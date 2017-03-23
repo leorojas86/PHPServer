c408beffe6b05404ee50ff8e6cb7636433ecc723
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-03-22 20:56:06 -0600

    Revert "Implementing PHP server"
    This reverts commit 8a362ccc9f9af3989447923504d68d773f5fef14.

--- clients/html_client/js/components/image_container_component.js
+++ clients/html_client/js/components/image_container_component.js
@@ -8,7 +8,7 @@ function ImageContainerComponent(canvas)
 	function onImageLoadedCallback(resultData)
 	{
 		if(resultData.success)
-			ImageRenderingUtils.instance.renderImageBinaryStringIntoCanvas(resultData.data, canvas, canvas.parentElement.offsetWidth * 0.9, Constants.IMAGE_MAX_SIZE);
+			ImageRenderingUtils.instance.renderImageDataIntoCanvas(resultData.data, canvas, canvas.parentElement.offsetWidth * 0.9, Constants.IMAGE_MAX_SIZE);
 		else
 			alert('error downloading image ' + imageURL);
 	}
