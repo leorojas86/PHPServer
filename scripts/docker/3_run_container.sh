60c362a5b7b0ef788e6fe61ed8b09be64623f2cd
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-04-12 23:21:15 -0600

    Implementing PHP server

--- scripts/docker/3_run_container.sh
+++ scripts/docker/3_run_container.sh
@@ -22,9 +22,9 @@ docker run \
 --detach \
 --name=phpservercontainer \
 --publish 80:80 phpserver \
-/bin/bash -c "service apache2 start | service mysql start"
+/bin/bash
 
-#
+#-c "service apache2 start | service mysql start"
 
 #--ip 192.168.0.180 \
 #--network=isolated_nw \
