225feccce3f731ebb951af0799f041aa5d5d0230
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-11-15 15:41:20 -0600

    Implementing PHP server

---
+++ clients/html_client/cmvc/ui_component.js
@@ -0,0 +1,15 @@
+function UIComponent()
+{
+	//Variables
+	this.model = null;
+	this.view = null;
+	this.controller = null;
+	var _templatePrivateVariable = null;
+
+	//Methods
+	this.buildUI = function()
+	{
+		
+	};
+}
+
