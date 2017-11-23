3aaa4592457d6c0efe55b552d4cf43998ec5f278
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-11-23 10:19:40 -0600

    Implementing PHP server

--- clients/html_client/cmvc/ui_component.js
+++ clients/html_client/cmvc/ui_component.js
@@ -1,37 +1,22 @@
-function AppView() {
 
-	this.build = () => 
-	{
-		headerController.build();
-		bodyController.build();
-		footerController.build();
-	};
-}
-
-
-function AppController()
+function App()
 {
 	//Variables
 	this.model = {
 		user: null,
 		currentScreen: null
 	};
-
 	this.children = [new Header(), new Body(), new Footer(), new Modals()];
 
+	//Methods
 	this.view = (model) => {
 		this.children.foreach((child) => {
 			child.view();
 		});
 	};
-
-	//Methods
-	this.buildUI = () => {
-		return this.view.build(elementId);
-	};
 }
 
-var App = new AppController();
+var App = new App();
 
 /*
 
