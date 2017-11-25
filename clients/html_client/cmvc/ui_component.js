257d73196eca749b05e5bb29356a8d6d809fadfc
Leonardo Rojas Jimenez <leonardo.rojas@agilysys.com>
2017-11-25 07:48:16 -0600

    Implementing PHP server

--- clients/html_client/cmvc/ui_component.js
+++ clients/html_client/cmvc/ui_component.js
@@ -1,26 +1,56 @@
 
-function App()
+class AppModel()
 {
-	//Variables
-	this.model = {
-		user: null,
-		currentScreen: null
-	};
-	this.children = [new Header(), new Body(), new Footer(), new Modals()];
+	constructor()
+	{
+		this.data = { user: null, currentScreen: null };
+	}
+}
+
+class AppView()
+{
+	constructor()
+	{
+		this.html =
+			`<div id="header" />
+			 <div id="screen" />
+			 <div id="footer" />
+			 <div id="modals" />`;
+	}
+
+	build(children)
+	{
+		const body = document.getElementsByTagName('body')[0];
+		body.innerHTML = this.html;
+		children.foreach((child) => child.view.build(this.model));
+	}
+}
+
+
+class App()
+{
+	constructor()
+	{
+		this.model = new AppModel();
+		this.view = new AppView();
+		this.children = [new Header(), new Screen(), new Footer(), new Modals()];
+	}
 
 	//Methods
-	this.view = () => {
-		this.children.foreach((child) => child.view(this.model));
-	};
+	buildUI()
+	{
+		this.view.build(this.children);
+	}
 }
 
 var App = new App();
+App.buildUI();
 
 /*
 
-App 
+App
 	- Header
-		- PageTitle
+		- ScreenTitle
 		- ScreenLinks
 			- Home
 			- Inventory
