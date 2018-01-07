70b753f7ed1ce646627ccf0dadfad52356906fb1
Leo <leonardo.rojas@agilysys.com>
2018-01-06 21:30:23 -0600

    Implementing PHP server

--- clients/html_client/app/components/header/Header.js
+++ clients/html_client/app/components/header/Header.js
@@ -20,10 +20,13 @@ class HeaderView {
     const user = this.component.model.user;
     return `<div id='${this.id}' class='${this.id}'>
               <div class='right_buttons_container'>
-                <button id='${this.id}_notifications_button' class='user_button'>
+                <button id='${this.id}_options_button'>
+                  <span class="lsf symbol">menu</span>
+                </button>
+                <button id='${this.id}_notifications_button'>
                   <span class="lsf symbol">globe</span>
                 </button>
-                <button id='${this.id}_user_button' class='user_button'>
+                <button id='${this.id}_user_button'>
                   <span class="lsf symbol">${ user ? '' : 'in' }user</span>
                 </button>
               </div>
