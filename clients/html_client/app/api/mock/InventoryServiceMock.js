31183101eece5e57b99b4a2e20f64a7cb0ecdb90
Leo <leonardo.rojas@agilysys.com>
2018-01-13 14:40:00 -0600

    Implementing PHP server

--- clients/html_client/app/api/mock/InventoryServiceMock.js
+++ clients/html_client/app/api/mock/InventoryServiceMock.js
@@ -4,8 +4,9 @@ class InventoryServiceMock {
 
     this.items = [
       { id:'0', name:'#', path:['#'], type:'folder', children:['1','2'], pathIds:['0'] },
-      { id:'1', name:'folder 1', path:['#', 'folder 1'], type:'folder', children:[], pathIds:['0','1'] },
-      { id:'2', name:'file 1', path:['#', 'file 1'], type:'file', pathIds:['0','2'] },
+      { id:'1', name:'folder 1', path:['#', 'folder 1'], type:'folder', children:['3'], pathIds:['0'] },
+      { id:'2', name:'file 1', path:['#', 'file 1'], type:'file', pathIds:['0'] },
+      { id:'3', name:'folder 2', path:['#', 'folder 1', 'folder 2'], type:'folder', children:[], pathIds:['0','1'] },
     ];
     this.mockEnvironment = Environments.get()['mock'];
     this.responseMiliSec = this.mockEnvironment.responseSec * 1000;
