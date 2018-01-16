4059d2048122765cfedb6254a3502c4e8dd3bead
Leo <leonardo.rojas@agilysys.com>
2018-01-15 21:50:51 -0600

    Implementing PHP server

---
+++ clients/html_client/app/components/screens/inventory/inventory_folder/InventoryContextMenuHelper.js
@@ -0,0 +1,50 @@
+class InventoryContextMenuHelper {
+
+  constructor() {
+    this.cutingItemId = null;
+  }
+
+  registerOnContextMenuEvent(inventoryElement) {
+    inventoryElement.oncontextmenu = (event) => {
+      const itemType = event.target.getAttribute('itemType');
+      const itemId = event.target.getAttribute('itemId');
+      App.instance.contextMenu.show(this.getMenuOptions(itemType, itemId), { x:event.clientX, y:event.clientY });
+      return false;
+    }
+  }
+
+  getMenuOptions(itemType, itemId) {
+    switch (itemType) {
+      case 'folder':
+      case 'file':
+        return [
+          { id:'rename', text:'[@rename_text@]', onClick: () => this.onClick('rename', itemId) },
+          { id:'cut', text:'[@cut_text@]', onClick: () => this.onClick('cut', itemId) },
+          { id:'delete', text:'[@delete_text@]', onClick: () => this.onClick('delete', itemId) }
+        ];
+      break;
+      default:
+        const defaultOptions = [
+          { id:'add_file', text:'[@add_file_text@]', onClick: () => this.onClick('add_file', itemId) },
+          { id:'add_folder', text:'[@add_folder_text@]', onClick: () => this.onClick('add_folder', itemId) }
+        ];
+        if(this.cutingItemId) {
+          defaultOptions.push({ id:'paste', text:'[@paste_text@]', onClick: () => this.onClick('paste', itemId) });
+        }
+        return defaultOptions;
+      break;
+    }
+  }
+
+  onClick(action, itemId) {
+    switch (action) {
+      case 'cut': this.cutingItemId=itemId; break;
+      case 'rename':  break;
+      case 'delete':  break;
+      case 'add_file':  break;
+      case 'add_folder':  break;
+      case 'paste':  break;
+    }
+  }
+
+}
--- clients/html_client/app/components/screens/inventory/inventory_folder/InventoryFolder.js
+++ clients/html_client/app/components/screens/inventory/inventory_folder/InventoryFolder.js
@@ -2,7 +2,6 @@ class InventoryFolderModel {
 
   constructor(component) {
     this.children = null;
-    this.cutingItemId = null;
     this.component = component;
   }
 
@@ -12,37 +11,6 @@ class InventoryFolderModel {
       .then((children) => this.children = children);
   }
 
-  getMenuOptions(itemType, itemId) {
-    switch (itemType) {
-      case 'folder':
-      case 'file':
-        return [
-          { id:'rename', text:'[@rename_text@]', onClick: () => this.onClick('rename', itemId) },
-          { id:'cut', text:'[@cut_text@]', onClick: () => this.onClick('cut', itemId) },
-          { id:'delete', text:'[@delete_text@]', onClick: () => this.onClick('delete', itemId) }
-        ];
-      break;
-      default:
-        const defaultOptions = [
-          { id:'add_file', text:'[@add_file_text@]', onClick: () => this.onClick('add_file', itemId) },
-          { id:'add_folder', text:'[@add_folder_text@]', onClick: () => this.onClick('add_folder', itemId) }
-        ];
-
-        if(this.cutingItemId) {
-          defaultOptions.push({ id:'paste', text:'[@paste_text@]', onClick: () => this.onClick('paste', itemId) });
-        }
-
-        return defaultOptions;
-      break;
-    }
-  }
-
-  onClick(action, itemId) {
-    switch (action) {
-      case 'cut': this.cutingItemId=itemId; break;
-      default: this.component.onContextMenuOptionClicked(action, itemId); break;
-    }
-  }
 }
 
 class InventoryFolderView {
@@ -62,13 +30,7 @@ class InventoryFolderView {
 
   onDomUpdated() {
     this.component.children.forEach((child) => child.view.onDomUpdated());
-    const element = document.getElementById(this.id);
-    element.oncontextmenu = (event) => {
-      const itemType = event.target.getAttribute('itemType');
-      const itemId = event.target.getAttribute('itemId');
-      App.instance.contextMenu.show(this.component.model.getMenuOptions(itemType, itemId), { x:event.clientX, y:event.clientY });
-      return false;
-    }
+    this.component.contextMenuHelper.registerOnContextMenuEvent(document.getElementById(this.id));
   }
 
 }
@@ -79,6 +41,7 @@ class InventoryFolder {
     this.model = new InventoryFolderModel(this);
     this.view = new InventoryFolderView(this);
     this.children = [];
+    this.contextMenuHelper = new InventoryContextMenuHelper();
   }
 
   load() {
@@ -87,18 +50,6 @@ class InventoryFolder {
       .then(() => {
         this.model.children.forEach((child) => this.children.push(new InventoryFolderChild(`${child.id}`, child)));
       });
-  }
-
-  onContextMenuOptionClicked(action, itemId) {
-    let actionFunction = null;
-
-    switch (action) {
-      case 'rename':  break;
-      case 'delete':  break;
-      case 'add_file':  break;
-      case 'add_folder':  break;
-      case 'paste':  break;
-    }
   }
 
 }
