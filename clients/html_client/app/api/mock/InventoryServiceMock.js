4f7061cb3703ef3eb15729d7907ab3feb29f6f66
Leo <leonardo.rojas@agilysys.com>
2018-01-17 20:31:22 -0600

    Implementing PHP server

--- clients/html_client/app/api/mock/InventoryServiceMock.js
+++ clients/html_client/app/api/mock/InventoryServiceMock.js
@@ -2,10 +2,10 @@ class InventoryServiceMock {
 
   constructor() {
     this.items = [
-      { id:'0', name:'#', type:'folder', children:['1','2'], path:['#'], pathIds:['0'] },
-      { id:'1', name:'folder 1', type:'folder', children:['3'], path:['#', 'folder 1'], pathIds:['0'] },
-      { id:'2', name:'file 1', type:'file', path:['#', 'file 1'], pathIds:['0'] },
-      { id:'3', name:'folder 2', type:'folder', children:[], path:['#', 'folder 1', 'folder 2'], pathIds:['0','1'] },
+      { id:'0', name:'#', type:'folder', children:['1','2'], path:['#'], pathIds:['0'], parentId:null },
+      { id:'1', name:'folder 1', type:'folder', children:['3'], path:['#', 'folder 1'], pathIds:['0'], parentId:'0' },
+      { id:'2', name:'file 1', type:'file', path:['#', 'file 1'], pathIds:['0'], parentId:'0' },
+      { id:'3', name:'folder 2', type:'folder', children:[], path:['#', 'folder 1', 'folder 2'], pathIds:['0','1'], parentId:'1' },
     ];
     this.mockEnvironment = Environments.get()['mock'];
     this.responseMiliSec = this.mockEnvironment.responseSec * 1000;
@@ -25,6 +25,15 @@ class InventoryServiceMock {
     });
   }
 
+  getItemById(id) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const item = this.items.find((currentItem) => currentItem.id === id);
+        resolve(item);
+      }, this.responseMiliSec);
+    });
+  }
+
   getItemChildren(item) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
@@ -39,13 +48,8 @@ class InventoryServiceMock {
     });
   }
 
-  getItemById(id) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const item = this.items.find((currentItem) => currentItem.id === id);
-        resolve(item);
-      }, this.responseMiliSec);
-    });
+  getItemPath(item) {
+
   }
 
   deleteItem(id) {
@@ -67,7 +71,7 @@ class InventoryServiceMock {
         path.push(name);
         const pathIds = JSON.parse(JSON.stringify(parentItem.pathIds));
         pathIds.push(id);
-        this.items.push({ id:id, name:name, type:type, children:[], path:path, pathIds:pathIds });
+        this.items.push({ id:id, name:name, type:type, children:[], path:path, pathIds:pathIds, parentId:parentItem.id });
         parentItem.children.push(id);
         resolve();
       }, this.responseMiliSec);
