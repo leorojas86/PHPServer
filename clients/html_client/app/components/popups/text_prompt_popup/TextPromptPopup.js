3b86bde7d5717328e2ca048bf20ab60534eccc5c
Leo <leonardo.rojas@agilysys.com>
2018-01-17 22:31:39 -0600

    Implementing PHP server

--- clients/html_client/app/components/popups/text_prompt_popup/TextPromptPopup.js
+++ clients/html_client/app/components/popups/text_prompt_popup/TextPromptPopup.js
@@ -20,28 +20,34 @@ class TextPromptPopupView {
                <p>
                 <input type='text' id='${this.id}_input_text' placeholder='${data.placeholder}' value='${data.value ? data.value : ''}'>
                </p>
-               <button id='${this.id}_ok_button'>
-                 <span class="lsf symbol">ok</span> [@ok_text@]
-               </button>
                <button id='${this.id}_cancel_button'>
                  <span class="lsf symbol">close</span> [@cancel_text@]
+               </button>
+               <button id='${this.id}_ok_button'>
+                 <span class="lsf symbol">ok</span> [@ok_text@]
                </button>
              </div>`;
   }
 
   onDomUpdated() {
-    Html.registerMouseDown(`${this.id}_grayout`, () => {});//Do nothing
-    Html.registerClick(`${this.id}_ok_button`, () => {
-      const inputValue = Html.getValue(`${this.id}_input_text`);
-      this.component.popup.hide();
-      this.component.model.data.onTextEntered(inputValue);
-    });
+    Html.registerMouseDown(`${this.id}_grayout`, () => {});//Do not automatically close modal when clicked outside the modal
+    Html.registerClick(`${this.id}_ok_button`, () => this.submit());
     Html.registerClick(`${this.id}_cancel_button`, () => this.component.popup.hide());
     Html.setDisabled(`${this.id}_ok_button`, true);
-    Html.onKeyUp(`${this.id}_input_text`, () => {
-      const inputValue = Html.getValue(`${this.id}_input_text`);
-      Html.setDisabled(`${this.id}_ok_button`, !inputValue);
+    Html.onKeyUp(`${this.id}_input_text`, (key) => {
+      if(key.code === 'Enter') {
+        this.submit();
+      } else {
+        const inputValue = Html.getValue(`${this.id}_input_text`);
+        Html.setDisabled(`${this.id}_ok_button`, !inputValue);
+      }
     });
+  }
+
+  submit() {
+    const inputValue = Html.getValue(`${this.id}_input_text`);
+    this.component.popup.hide();
+    this.component.model.data.onTextEntered(inputValue);
   }
 
 }
