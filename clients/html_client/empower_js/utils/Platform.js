eccc93d90f69a469210f5f1c3dd7d19edaa4e913
Leo <leonardo.rojas@agilysys.com>
2018-01-28 12:03:58 -0600

    Implementing PHP server

--- clients/html_client/empower_js/utils/Platform.js
+++ clients/html_client/empower_js/utils/Platform.js
@@ -1,14 +1,10 @@
 class Platform {
   static init() {
-    if(Platform.isiOS()) {//.finally is not supported by promise for iOS: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/finally
+    if(!Promise.prototype.finally) {//.finally is not supported by promise for iOS: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/finally
       Promise.prototype.finally = function(callback) {
         return this.then(callback)
           .catch(callback);
       };
     }
-  }
-
-  static isiOS() {
-    return /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
   }
 }
