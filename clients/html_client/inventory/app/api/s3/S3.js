43ee385395dccd1993a61ef05cfb0b45fcde96c8
Leo <leonardo.rojas@agilysys.com>
2018-01-31 21:49:22 -0600

    WIP

---
+++ clients/html_client/inventory/app/api/s3/S3.js
@@ -0,0 +1,19 @@
+class S3 {
+    constructor() {
+      this.s3 = null;
+    }
+
+    initialize(albumBucketName, bucketRegion, IdentityPoolId) {
+      AWS.config.update({
+        region: bucketRegion,
+        credentials: new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: IdentityPoolId
+        })
+      });
+
+      this.s3 = new AWS.S3({
+        apiVersion: '2006-03-01',
+        params: {Bucket: albumBucketName}
+      });
+    }
+}
