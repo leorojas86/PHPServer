ea5d7e63bc034e48443cebf7f24728a189fe099b
Leo <leonardo.rojas@agilysys.com>
2018-02-04 18:29:31 -0600

    Implementing PHP server

--- clients/html_client/inventory/app/data/Environments.js
+++ clients/html_client/inventory/app/data/Environments.js
@@ -6,7 +6,7 @@ class Environments {
       },
       dev: {
         s3: {
-          bucketName: 'inventory-db'
+          bucketName: 'inventory-db',
           bucketRegion: 'us-east-1',
           identityPoolId: 'us-east-1:5b88dd8b-b0d4-48a8-81bf-46189bb8877c'
         }
