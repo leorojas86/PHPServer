c38521654a982d9b8b14664d5656d8af5a0c3556
Leo <leonardo.rojas@agilysys.com>
2018-02-10 21:02:45 -0600

    Implementing PHP server

--- clients/html_client/inventory/app/api/s3/InventoryServiceS3.js
+++ clients/html_client/inventory/app/api/s3/InventoryServiceS3.js
@@ -3,11 +3,9 @@ class InventoryServiceS3 {
   _getPathItemRecursively(item) {
     const pathItem = { id:item.id, name:item.name };
     if(item.parentId) {
-      return getItemById(item.parentId)
-        .then((parentItem) => {
-          return this._getPathItemRecursively(parentItem)
-            .then((path) => path.push(pathItem));
-        });
+      return this.getItemById(item.parentId)
+        .then((parentItem) => this._getPathItemRecursively(parentItem))
+        .then((path) => path.push(pathItem));
     }
     return Promise.resolve([pathItem]);
   }
@@ -26,43 +24,39 @@ class InventoryServiceS3 {
   }
 
   deleteItem(item) {
-    getItemById(item.parentId)
+    return this.getItemById(item.parentId)
       .then((parentItem) => {
         parentItem.children.splice(parentItem.children.indexOf(item.id), 1);//Delete from parent children
-        return S3.instance.saveItem(`item_${item.parentId}`, parentItem);
+        return saveItem(parentItem);
       })
       .then(() => S3.instance.deleteItem(item.id));
   }
 
   addChildItem(type, name, parentItem) {
     const newItem = { id:GUIDUtils.generateNewGUID(), name:name, type:type, parentId:parentItem.id, children:[] };
-    saveItem(newItem)
-      .then(() => {
-        parentItem.children.push(newItem.id);
-        return S3.instance.saveItem(parentItem);
-      });
+    parentItem.children.push(newItem.id);
+    return this.saveItem(newItem)
+      .then(() => this.saveItem(parentItem));
   }
 
-  /*renameItem(item, newName) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        item.name = newName;
-        resolve();
-      }, this.responseMiliSec);
-    });
+  renameItem(item, newName) {
+    item.name = newName;
+    return this.saveItem(item);
   }
 
   moveItem(item, newParent) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const parentItem = this.items.find((currentItem) => currentItem.id === item.parentId);
-        parentItem.children.splice(parentItem.children.indexOf(item.id), 1);
-        newParent.children.push(item.id);
+    return this.getItemById(item.parentId)
+      .then((parentItem) => {
+        parentItem.children.splice(parentItem.children.indexOf(item.id), 1);//Delete from parent children
+        return this.saveItem(parentItem);
+      })
+      .then(() => {
         item.parentId = newParent.id;
-        resolve();
-      }, this.responseMiliSec);
-    });
-  }*/
+        newParent.children.push(item.id);
+        return this.saveItem(item)
+          .then(() => this.saveItem(newParent))
+      });
+  }
 
   saveItem(item) {
     return S3.instance.saveItem(`item_${item.id}`, item);
