d1ae4b5eaef5de6d6c3b4af7a01270fd860f75d0
Leo <leonardo.rojas@agilysys.com>
2018-02-13 22:02:08 -0600

    Implementing PHP server

--- clients/html_client/empower_js/components/dropdown_menu/DropdownMenu.js
+++ clients/html_client/empower_js/components/dropdown_menu/DropdownMenu.js
@@ -46,8 +46,10 @@ class DropdownMenuView {
 
   setPosition(position) {
     const element = Html.getElement(`${this.id}_options`);
-    const maxX = window.innerWidth - element.offsetWidth;
-    const maxY = window.innerHeight - element.offsetHeight;
+    const windowScrolledXOffset = window.pageXOffset || document.documentElement.scrollLeft;
+		const windowScrolledYOffset = window.pageYOffset || document.documentElement.scrollTop;
+    const maxX = window.innerWidth + windowScrolledXOffset - element.offsetWidth;
+    const maxY = window.innerHeight + windowScrolledYOffset - element.offsetHeight;
     position.x = position.x < maxX ? position.x : maxX;
     position.y = position.y < maxY ? position.y : maxY;
     element.style.position = "absolute";
