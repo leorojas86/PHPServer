e3497b5970fd5bd87d504b56168ad822ee0330b9
Leo <leonardo.rojas@agilysys.com>
2018-02-25 19:48:38 -0600

    Implementing PHP server

--- clients/html_client/super/app/components/screens/cart/Cart.js
+++ clients/html_client/super/app/components/screens/cart/Cart.js
@@ -39,7 +39,7 @@ class CartView {
     let cartItemsRows = '';
     this.component.model.cartItems.forEach((cartItem) => {
       cartItemsRows += `<tr>
-                          <th>${cartItem.quantity + cartItem.unit}</th><th>${cartItem.description}</th><th>${cartItem.pricePerUnit}</th><th>${cartItem.quantity * cartItem.pricePerUnit}</th>
+                          <th>${cartItem.quantity} ${cartItem.unit}</th><th>${cartItem.description}</th><th>${cartItem.pricePerUnit}</th><th>${cartItem.quantity * cartItem.pricePerUnit}</th>
                         </tr>`;
     });
     return `<div id='${this.id}' class='${this.id}'>
